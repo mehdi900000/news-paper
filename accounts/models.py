938bcc84343da75de65f2e74a92814590df7ff15
Mehdi900000 <Mehdi900000@email.com>
2025-09-21 15:41:13 +0330

    add project

---
+++ accounts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    age=models.PositiveBigIntegerField(null=True,blank=True)
